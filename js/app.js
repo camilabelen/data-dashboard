a5b0b93af702a67ce6decd68efe5b0e3269ceeb1
mina <romina.torres.b@gmail.com>
2017-11-19 11:08:10 -0300

    subiendo archivo

--- js/app.js
+++ js/app.js
@@ -2,5 +2,31 @@
  * Funcionalidad de tu producto
  */
 
+
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
+
+google.charts.load('current', {packages: ['corechart']});
+      function drawChart ()
+      {
+      	var data  = new google.visualization.DataTable();/* creando una tabla de datos */
+      	data.addColumn('string','Alumnas');
+      	data.addColumn('number','Porcentaje');
+      	data.addRows(
+      		[
+      		['Alumnas sobre',90],
+      		['alumnas de',70],
+      		['alumnas bajo'70]
+      		]
+      		);
+
+      	var titulos = {'title':'Nivel Tecnico'};
+      	var tipo = new google.visualization.AreaChart(document.getElementById('ntecnico'));
+      	tipo.draw(data,titulos);
+
+
+}
+google.charts.setOnLoadCallback(drawChart);
+
+      
